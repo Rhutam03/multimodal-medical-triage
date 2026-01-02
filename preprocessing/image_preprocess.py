651bb9cf4b7305dd304992393f785f63b21d4996
Rhutam Mahajan <rhutammahajan@gmail.com>
2026-01-02 15:59:48 +0530

    Fix image preprocessing function and import statement

--- preprocessing/image_preprocess.py
+++ preprocessing/image_preprocess.py
@@ -1,21 +1,12 @@
-from torchvision import transforms
 from PIL import Image
+import torchvision.transforms as transforms
 
-
-# ImageNet-style preprocessing
-image_transform = transforms.Compose([
+transform = transforms.Compose([
     transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]
-    )
+    transforms.ToTensor()
 ])
 
-
 def preprocess_image(image_path):
-    """
-    Loads an image from disk and applies preprocessing.
-    """
     image = Image.open(image_path).convert("RGB")
-    return image_transform(image)
+    return transform(image)
+
