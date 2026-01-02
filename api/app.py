1771587f0e51a51566184bd81cc757e5a253fdbf
Rhutam Mahajan <rhutammahajan@gmail.com>
2026-01-02 16:26:53 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -13,4 +13,4 @@ async def predict_api(
     with open(image_path, "wb") as f:
          f.write(await image.read())
     triage_level = predict(image_path, text)
-    return {"triage_level": triage_level)}
+    return {"triage_level": triage_level}
