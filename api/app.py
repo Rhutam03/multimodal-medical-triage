a573cd5d2f974133d01507ea0420904867c55209
Rhutam Mahajan <rhutammahajan@gmail.com>
2026-01-03 18:46:02 +0530

    Enhance FastAPI app with title and update image path

--- api/app.py
+++ api/app.py
@@ -2,15 +2,25 @@ from fastapi import FastAPI, UploadFile, File, Form
 import shutil
 from core.inference import predict
 
-app = FastAPI()
+app = FastAPI(title="Multimodal Medical Triage API")
+
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
+
 
 @app.post("/predict")
 async def predict_api(
     image: UploadFile = File(...),
     text: str = Form(...)
 ):
-    image_path = f"temp_{image.filename}"
+    image_path = f"/tmp/{image.filename}"
+
     with open(image_path, "wb") as f:
-         f.write(await image.read())
+        shutil.copyfileobj(image.file, f)
+
     triage_level = predict(image_path, text)
+
     return {"triage_level": triage_level}
+
